02f4260b6ab1bc22608a0eb0222b63692b13d1bc
Ultimate <aimanyosofiult>
2022-05-09 11:12:12 +0200

    Aiman Commit5

--- app/src/main/java/com/ultimate/ecommerce/ui/fragment/category/CategoryFragmentViewModel.java
+++ app/src/main/java/com/ultimate/ecommerce/ui/fragment/category/CategoryFragmentViewModel.java
@@ -1,16 +1,12 @@
 package com.ultimate.ecommerce.ui.fragment.category;
 
 import android.app.Application;
-
 import androidx.annotation.NonNull;
 import androidx.lifecycle.LiveData;
-
 import com.ultimate.ecommerce.repository.local.tables.category.Category;
 import com.ultimate.ecommerce.repository.repos.category.CategoryRepo;
 import com.ultimate.ecommerce.ui.base.BaseViewModel;
-
 import java.util.List;
-
 import javax.inject.Inject;
 
 public class CategoryFragmentViewModel extends BaseViewModel {
