9ddaf41dc31159cfd1750e8e603709e30baf6808
edit-1 <1>
2022-05-24 13:27:53 +0200

    Aiman Commit M12

--- app/src/main/java/com/ultimate/ecommerce/ui/base/BaseDialog.java
+++ app/src/main/java/com/ultimate/ecommerce/ui/base/BaseDialog.java
@@ -4,15 +4,12 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.view.View;
 
-import androidx.lifecycle.LifecycleOwner;
-
-import javax.inject.Inject;
-
 public abstract class BaseDialog extends AlertDialog.Builder {
     protected AlertDialog dialog;
 
     protected BaseDialog(Context context) {
         super(context);
+        setCancelable(true);
     }
 
     @Override
